33db49e76bb1aab53a2c58ef4063368844bd18d8
mawit <mawitgaddev@gmail.com>
2023-03-08 09:35:59 +0100

    created the first view of the home page

---
+++ assets/images.ts
@@ -0,0 +1,7 @@
+export const litumbaPreviewProject = "/images/litumba-preview-project.jpg";
+export const litumbaPreviewProjects = "/images/litumba-preview-projects.jpg";
+export const ndoloPreviewProject = "/images/ndolo-preview-project.jpg";
+export const ndoloPreviewProjects = "/images/ndolo-preview-projects.jpg";
+export const personalWebsitePreviewProject = "/images/personal-website-preview-project.jpg";
+export const personalWebsitePreviewProjects = "/images/personal-website-preview-projects.jpg";
+export const avatar = "/images/avatar.svg";
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./app/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			boxShadow: {
+				project_shadow:
+					"394px 264px 190px rgba(0, 0, 0, 0.03), 222px 148px 160px rgba(0, 0, 0, 0.1), 99px 66px 119px rgba(0, 0, 0, 0.16), 25px 16px 65px rgba(0, 0, 0, 0.19), 0px 0px 0px rgba(0, 0, 0, 0.19);",
+			},
+		},
+	},
+	plugins: [],
+};
