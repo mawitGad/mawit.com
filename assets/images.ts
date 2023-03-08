8e7c70b4058c134c30c5318ff00cf0316cf6ec11
mawit <mawitgaddev@gmail.com>
2023-03-08 15:07:22 +0100

    modified images forlder in attenmpt to solve the image not serving issues

--- assets/images.ts
+++ assets/images.ts
@@ -1,7 +1,7 @@
-export const litumbaPreviewProject = "/images/litumba-preview-project.jpg";
-export const litumbaPreviewProjects = "/images/litumba-preview-projects.jpg";
-export const ndoloPreviewProject = "/images/ndolo-preview-project.jpg";
-export const ndoloPreviewProjects = "/images/ndolo-preview-projects.jpg";
-export const personalWebsitePreviewProject = "/images/personal-website-preview-project.jpg";
-export const personalWebsitePreviewProjects = "/images/personal-website-preview-projects.jpg";
-export const avatar = "/images/avatar.svg";
+export const litumbaPreviewProject = "/image-stock/litumba-preview-project.jpg";
+export const litumbaPreviewProjects = "/image-stock/litumba-preview-projects.jpg";
+export const ndoloPreviewProject = "/image-stock/ndolo-preview-project.jpg";
+export const ndoloPreviewProjects = "/image-stock/ndolo-preview-projects.jpg";
+export const personalWebsitePreviewProject = "/image-stock/personal-website-preview-project.jpg";
+export const personalWebsitePreviewProjects = "/image-stock/personal-website-preview-projects.jpg";
+export const avatar = "/image-stock/avatar.svg";
