8899023c25e24769a0c2e40e0bd9c0ecfbff7b1d
Mawit Gad <mawitgaddev@gmail.com>
2023-03-14 06:40:53 +0000

    re-established original title

--- pages/index.tsx
+++ pages/index.tsx
@@ -69,8 +69,7 @@ export default function Home() {
                   to my personal website, I am
                 </span>
                 <br />
-                Mawit Bikom Gad, A full-stack web developer with react and next
-                js with typsctipt too how do you mean there are no changes???
+                Mawit Bikom Gad, A full-stack web developer
               </h3>
               <p className="normal-text mt-[50px] max-[632px]:text-[15px] max-[1120px]:mt-[25px]">
                 I am a self-taught full-stack web developer with a passion for
