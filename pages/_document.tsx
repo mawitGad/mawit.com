db17480aaadb74ae48c38bad31ab5adfe55ec6f5
mawitGad <107782949+mawitGad@users.noreply.github.com>
2025-01-08 13:58:50 +0100

    added google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,16 @@ export default function Document() {
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
                 <link href="https://fonts.googleapis.com/css2?family=Antonio&family=Roboto+Mono:wght@300&display=swap" rel="stylesheet" />
+                
+                <!-- Google tag (gtag.js) -->
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-21YYHLM69N"></script>
+                <script>
+                {window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-21YYHLM69N');}
+                </script>
             </Head>
             <body>
                 <Main />
